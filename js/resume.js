23f4cb364576f8cf77de87b85af381d7ceb7566d
zhouchen <im.zhouchen@foxmail.com>
2017-03-14 01:20:11 +0800

    调整并完善页面元素样式

--- js/resume.js
+++ js/resume.js
@@ -5,6 +5,9 @@
     $('.resume .detail .nav').on('click', 'li', function() {
         var selectedNav = $(this);
         selectedNav.addClass('active');
+        selectedNav.siblings().each(function() {
+            $(this).removeClass('active');
+        });
     });
 
 })(Elements);
